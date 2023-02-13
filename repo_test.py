172f978cb7fc96fb9a96ec1f7d4509dad1065517
Max Goldstein <goldstem@oregonstate.edu>
2023-02-12 20:25:14 -0800

    first commit

--- repo_test.py
+++ repo_test.py
@@ -1,7 +1,28 @@
-import subprocess
+import requests
+import json
+import socket
 
-subprocess.call(["git", "init"])
-subprocess.call(["git", "add", "--all"])
-subprocess.call(["git", "add", "README.md"])
-subprocess.call(["git", "commit", "-m", "test"])
-#subprocess.call(["git", "push"])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((socket.gethostname(), 9876))
+
+msg = s.recv(1024).decode('utf-8')
+msg = json.loads(msg)
+
+print(msg)
+
+pat = "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"
+api = "https://api.github.com"
+user = "goldstem"
+
+response = requests.post(api + "/user/repos", data=json.dumps(msg), headers={"Authorization": "token " + pat})
+response.raise_for_status()
+
+repo_link = "https://github.com/" + user + "/" + msg["name"]
+
+if response.status_code == 201:
+    new_msg = s.send(("Created repository at: " + repo_link).encode('utf-8'))
+else:
+    print("Error: repository creation failed")
+
+
--- server.py
+++ server.py
@@ -1,7 +1,27 @@
-from flask import Flask
+import socket
+import json
 
-app = Flask(__name__)
 
-@app.route("/")
-def test():
-    return "test"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = 9876
+s.bind((socket.gethostname(), port))
+
+s.listen(5)
+
+data = {
+    "name": "demo_video",
+    "description": "test",
+    "private": True
+}
+
+while True:
+    clientsocket, address = s.accept()
+    print("Connected to " + str(address))
+
+    clientsocket.send(json.dumps(data).encode('utf-8'))
+
+    msg = clientsocket.recv(1024).decode('utf-8')
+    print(msg)
+
+    clientsocket.close()
